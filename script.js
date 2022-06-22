c97c522198cd24ea6e96e13810c634aaf9536fe5
aaronlrv <aaronvitharanage@gmail.com>
2022-06-22 17:43:04 +0400

    Add function to push books into library array

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 let myLibrary = [];
 
-function book(author, title, pages, status) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.status = status
+function book (author, title, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+} 
+
+let hobbit = new book ("J.R.R. Tolkien", "The hobbit", "72", "not read yet")
+console.log(hobbit)
+console.log( new book ("J.R.R. Tolkien", "The hobbit", "72", "not read yet"))
+
+
+book.prototype.addBookToLibrary = function () {
+    myLibrary.push(hobbit)
 }
 
-function addBookToLibrary() {
-  // do stuff here
-}
+console.log(hobbit.addBookToLibrary())
+console.log(myLibrary)
+
+
+
