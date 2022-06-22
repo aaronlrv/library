94a3dd6bbb981b91b849151c94a772d8cffecf4a
aaronlrv <aaronvitharanage@gmail.com>
2022-06-22 21:15:12 +0400

    Add event listener to grab form input

--- script.js
+++ script.js
@@ -20,4 +20,22 @@ console.log(hobbit.addBookToLibrary())
 console.log(myLibrary)
 
 
+document.getElementById("formID").addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    let bookName = document.getElementById("book-name").value
+    let author = document.getElementById("author-title").value
+    let pages = document.getElementById("pages").value
+    let status = document.getElementById("status").value
+
+    console.log(bookName)
+    console.log(author)
+    console.log(pages)
+    console.log(status)
+});
+
+
+    
+    
+
 
