252a6692a6ed9885035b13ee2d87ab863af7af48
aaronlrv <aaronvitharanage@gmail.com>
2022-06-22 16:44:38 +0400

    Create function to store book details

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function book(author, title, pages, status) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.status = status
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
