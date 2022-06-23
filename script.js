01c1f142138fefbb920d68d5a2113a2f948fb669
aaronlrv <aaronvitharanage@gmail.com>
2022-06-23 18:06:15 +0400

    Rewrote constructor to allow users to have libary displayed on cards

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
 let myLibrary = [];
 
 document.getElementById("formID").addEventListener("submit", (e) => {
+
     e.preventDefault()
     let title = document.getElementById("book-name").value
     let author = document.getElementById("author-title").value
     let pages = document.getElementById("pages").value
     let status = document.querySelector('input[name="status"]:checked').value;
+    let titleP = document.querySelector(".book-title")
+    let authorsP = document.querySelector(".authors-name")
+    let pagesP = document.querySelector (".js-pages")
+
+    class book {
+
+        constructor (title, author, pages, status) {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.status = status;
+    }
+    
+            cardMaker () {
+                console.log("This Works!")
+                titleP.textContent = this.title 
+                authorsP.textContent = this.author
+                pagesP.textContent = this.pages
+            }
+        } 
 
     
 
     let userBook = new book (title ,author , pages , status)
+    userBook.cardMaker()
     myLibrary.push(userBook)
 
 
-    function book (author, title, pages, status) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.status = status;
-    } 
+
     
     console.log(myLibrary)
+
 
 });
 
